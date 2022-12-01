a9dc56381fb3b08523ae3f75489eef8f20ca1d02
Jaineel Shah <jaineel@Jaineels-MacBook.local>
2022-12-01 16:37:57 -0500

    Sending file to EC2

--- cli/aws-ec2-support.py
+++ cli/aws-ec2-support.py
@@ -1,5 +1,7 @@
 import sys
 from pprint import pprint
+from paramiko import SSHClient
+from scp import SCPClient
 
 # Instance Id
 import boto3
@@ -9,6 +11,18 @@ Myec2 = client.describe_instances()
 for pythonins in Myec2["Reservations"]:
     for printout in pythonins["Instances"]:
         pprint(printout)
+
+        # Send script via SCP - "scp -i cpython.pem <path_to_script_file> ec2-user@ec2-3-89-101-123.compute-1.amazonaws.com:~/."
+        ssh = SSHClient()
+        ssh.load_system_host_keys()
+        ssh.connect('ec2-user@ec2-3-89-101-123.compute-1.amazonaws.com')
+        with SCPClient(ssh.get_transport()) as scp:
+            scp.put('script.py', 'script.py')
+
+        # Send command to execute the script
+        commands = ['python3 script.py']
+        instance_ids = ['an_instance_id_string']
+        execute_commands_on_linux_instances(client, commands, instance_ids)
 
 
 # print(sys.argv)
