6cf83385597d2c9cea33b7841ca89934c3927348
Jaineel Shah <jaineel@Jaineels-MacBook.local>
2022-12-08 11:12:10 -0500

    A Few Sample Scripts

--- scripts/pyperformance.sh
+++ scripts/pyperformance.sh
@@ -1,5 +1,5 @@
 
-
+# Running pyperformance on the given compiled python version
 run_pyperformance() {
     read -p "Enter path to the python : " path
     pyperformance run --python="$path" -o pyperformance_op.json
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,11 @@
 
 
 setup() {
+    # Reading the repo to be cloned
     read -p "Enter the repo link to be cloned : " link
     git clone "$link"
+
+    # Going inside the repo and installing it
     read -p "Enter path to the Repo folder:" name
     cd "$name"
     ./configure
